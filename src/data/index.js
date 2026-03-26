79bec629edf5beed3ce5c4df62f4dc79cd3b4e23
austinchia <austin.chia.ce@gmail.com>
2026-03-26 22:41:03 +0800

    feat: add React data layer importing FastF1 JSON files

---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+import races from './races.json';
+import bhr2024 from './bhr2024.json';
+import sau2024 from './sau2024.json';
+import aus2024 from './aus2024.json';
+
+export const RACES = races;
+
+export const RACE_DATA = { bhr2024, sau2024, aus2024 };
